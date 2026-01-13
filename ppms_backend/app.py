12d8e4d78ddeb99f9eb6aae3dcc9adb0b09bf569
jinsooKing <zkdlsem45@gmail.com>
2026-01-13 08:55:03 +0000

    정식배포

--- ppms_backend/app.py
+++ ppms_backend/app.py
@@ -61,6 +61,8 @@ def create_app():
 
     return app
 
+app = create_app()
 if __name__ == '__main__':
-    app = create_app()
     app.run(host='0.0.0.0', port=5000, debug=True)
+    
+# sudo systemctl restart ppms << 수정 후 서버 재시작 코드.
